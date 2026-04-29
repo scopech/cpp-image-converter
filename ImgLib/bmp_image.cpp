9b178966f616762f6292ad1910db9718af8a78a6
Egorov Dmitry <scopech@yandex.ru>
2026-04-29 23:57:58 +0300

    Update bmp_image.cpp
    after review

--- ImgLib/bmp_image.cpp
+++ ImgLib/bmp_image.cpp
@@ -11,31 +11,34 @@ using namespace std;
 namespace img_lib {
 
 PACKED_STRUCT_BEGIN BitmapFileHeader {
-    char signature[2];  
-    uint32_t size;       
-    uint32_t reserved;   
-    uint32_t offset;     
+    char signature[2];
+    uint32_t size;
+    uint32_t reserved;
+    uint32_t offset;
 }
 PACKED_STRUCT_END
 
 PACKED_STRUCT_BEGIN BitmapInfoHeader {
-    uint32_t header_size;     
-    int32_t width;            
-    int32_t height;           
-    uint16_t planes;          
-    uint16_t bit_count;       
-    uint32_t compression;     
-    uint32_t image_size;       
-    int32_t x_pels_per_meter;  
-    int32_t y_pels_per_meter;  
-    int32_t colors_used;       
-    int32_t colors_important;  
+    uint32_t header_size;
+    int32_t width;
+    int32_t height;
+    uint16_t planes;
+    uint16_t bit_count;
+    uint32_t compression;
+    uint32_t image_size;
+    int32_t x_pels_per_meter;
+    int32_t y_pels_per_meter;
+    int32_t colors_used;
+    int32_t colors_important;
 }
 PACKED_STRUCT_END
 
+//константы
+static const int BYTES_PER_PIXEL = 3;
+static const int ALIGNMENT = 4;
 
 static int GetBMPStride(int w) {
-    return 4 * ((w * 3 + 3) / 4);
+    return ALIGNMENT * ((w * BYTES_PER_PIXEL + ALIGNMENT - 1) / ALIGNMENT);
 }
 
 bool SaveBMP(const Path& file, const Image& image) {
@@ -48,41 +51,39 @@ bool SaveBMP(const Path& file, const Image& image) {
     const int h = image.GetHeight();
     const int stride = GetBMPStride(w);
 
-
     BitmapFileHeader file_header;
     file_header.signature[0] = 'B';
     file_header.signature[1] = 'M';
-    file_header.size = 14 + 40 + stride * h;
+    file_header.size = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + stride * h; //sizeof
     file_header.reserved = 0;
-    file_header.offset = 54; // 14 + 40
-
+    file_header.offset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
 
     BitmapInfoHeader info_header;
-    info_header.header_size = 40;
+    info_header.header_size = sizeof(BitmapInfoHeader);
     info_header.width = w;
     info_header.height = h;
     info_header.planes = 1;
-    info_header.bit_count = 24;
+    info_header.bit_count = BYTES_PER_PIXEL * 8; // 24 бита на пиксель
     info_header.compression = 0;
     info_header.image_size = stride * h;
     info_header.x_pels_per_meter = 11811;
     info_header.y_pels_per_meter = 11811;
     info_header.colors_used = 0;
-    info_header.colors_important = 0x1000000; 
+    info_header.colors_important = 0x1000000;
 
-    out.write(reinterpret_cast<const char*>(&file_header), 14);
-    out.write(reinterpret_cast<const char*>(&info_header), 40);
+    //sizeof
+    out.write(reinterpret_cast<const char*>(&file_header), sizeof(file_header));
+    out.write(reinterpret_cast<const char*>(&info_header), sizeof(info_header));
 
     std::vector<char> buff(stride, 0);
 
     for (int y = h - 1; y >= 0; --y) {
         const Color* line = image.GetLine(y);
         for (int x = 0; x < w; ++x) {
-            buff[x * 3 + 0] = static_cast<char>(line[x].b);
-            buff[x * 3 + 1] = static_cast<char>(line[x].g);
-            buff[x * 3 + 2] = static_cast<char>(line[x].r);
+            buff[x * BYTES_PER_PIXEL + 0] = static_cast<char>(line[x].b);
+            buff[x * BYTES_PER_PIXEL + 1] = static_cast<char>(line[x].g);
+            buff[x * BYTES_PER_PIXEL + 2] = static_cast<char>(line[x].r);
         }
-
         out.write(buff.data(), stride);
     }
 
@@ -98,10 +99,11 @@ Image LoadBMP(const Path& file) {
     BitmapFileHeader file_header;
     BitmapInfoHeader info_header;
 
-    if (!in.read(reinterpret_cast<char*>(&file_header), 14)) {
+    //sizeof
+    if (!in.read(reinterpret_cast<char*>(&file_header), sizeof(file_header))) {
         return {};
     }
-    if (!in.read(reinterpret_cast<char*>(&info_header), 40)) {
+    if (!in.read(reinterpret_cast<char*>(&info_header), sizeof(info_header))) {
         return {};
     }
 
@@ -116,16 +118,17 @@ Image LoadBMP(const Path& file) {
     Image result(w, h, Color::Black());
     std::vector<char> buff(stride);
 
+    
     for (int y = h - 1; y >= 0; --y) {
         if (!in.read(buff.data(), stride)) {
-            return {}; 
+            return {};
         }
 
         Color* line = result.GetLine(y);
         for (int x = 0; x < w; ++x) {
-            line[x].b = static_cast<std::byte>(buff[x * 3 + 0]);
-            line[x].g = static_cast<std::byte>(buff[x * 3 + 1]);
-            line[x].r = static_cast<std::byte>(buff[x * 3 + 2]);
+            line[x].b = static_cast<std::byte>(buff[x * BYTES_PER_PIXEL + 0]);
+            line[x].g = static_cast<std::byte>(buff[x * BYTES_PER_PIXEL + 1]);
+            line[x].r = static_cast<std::byte>(buff[x * BYTES_PER_PIXEL + 2]);
         }
     }
 
